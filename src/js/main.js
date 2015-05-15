7838ce5f4f6a133669fdebcd7c17895013e3f267
Thomas Wilburn <news@thomaswilburn.net>
2015-05-15 09:07:58 -0700

    Shading added.

--- src/js/main.js
+++ src/js/main.js
@@ -9,20 +9,70 @@ var bounds = loans.bounds;
 
 var augment = function(data) {
   data.ratio = {
-    clayton: data.clayton / bounds.clayton.max,
+    clayton: data.clayton / bounds.count.max,
     count: data.count / bounds.count.max,
     other: data.other / bounds.count.max
   }
 }
 
+var canvas = document.querySelector(".shader");
+var context = canvas.getContext("2d");
+
+var scaleRate = rate => (bounds.rate.max - rate) / (bounds.rate.max - bounds.rate.min + .5);
+var scaleAmount = amount => (amount - bounds.amount.min) / (bounds.amount.max + 5 - bounds.amount.min);
+
+var mode = "current";
+
+var redraw = function() {
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+  context.fillStyle = "rgba(0, 0, 0, .3)";
+  context.strokeStyle = "white";
+  context.setLineDash([5, 5]);
+  //create current path
+  context.beginPath();
+  context.moveTo(-10, scaleRate(8.5) * canvas.height);
+  context.lineTo(scaleAmount(50) * canvas.width, scaleRate(8.5) * canvas.height);
+  context.lineTo(scaleAmount(50) * canvas.width, scaleRate(6.5) * canvas.height);
+  context.lineTo(canvas.width + 10, scaleRate(6.5) * canvas.height);
+  context.lineTo(canvas.width + 10, -10);
+  context.lineTo(-10, -10);
+  if (mode == "current") {
+    //shade this area
+    console.log("filling current");
+    context.closePath();
+    context.fill();
+    return;
+  }
+  console.log("stroke current, fill proposed");
+  context.stroke();
+
+  //create proposed path
+  context.beginPath();
+  context.moveTo(-10, scaleRate(10) * canvas.height);
+  context.lineTo(scaleAmount(75) * canvas.width, scaleRate(10) * canvas.height);
+  context.lineTo(scaleAmount(75) * canvas.width, scaleRate(6.5) * canvas.height);
+  context.lineTo(canvas.width + 10, scaleRate(6.5) * canvas.height);
+  context.lineTo(canvas.width + 10, -10);
+  context.lineTo(-10, -10);
+  context.fill();
+};
+
+redraw();
+window.addEventListener("resize", redraw);
+
 loans.data.forEach(function(loan) {
+  if (loan.gap) return;
   augment(loan);
   var div = document.createElement("div");
   div.className = "cell";
-  div.style.top = (loan.rate - bounds.rate.min) / (bounds.rate.max + .5 - bounds.rate.min) * 100 + "%";
-  div.style.left = (loan.amount - bounds.amount.min) / (bounds.amount.max + 10 - bounds.amount.min) * 100 + "%";
+  var r = scaleRate(loan.rate);
+  div.style.top = Math.round(r * 1000) / 10 + "%";
+  var c = scaleAmount(loan.amount);
+  div.style.left = Math.round(c * 1000) / 10 + "%";
   div.setAttribute("data-rate", loan.rate);
   div.setAttribute("data-amount", loan.amount);
+  div.setAttribute("data-count", loan.count);
   //cover loans that happen
   if (loan.gap) {
     div.classList.add("gap");
@@ -42,23 +92,25 @@ loans.data.forEach(function(loan) {
       }
     }
   }
-  //add the protection shading
-  if ((loan.amount < 50 && loan.rate > 8.5) || (loan.amount >= 50 && loan.rate > 6.5)) {
-    div.classList.add("current");
-  }
-  if ((loan.amount < 75 && loan.rate > 10) || (loan.amount >= 75 && loan.rate > 6.5)) {
-    div.classList.add("proposed");
-  }
+  // add the protection shading
+  // if ((loan.amount < 50 && loan.rate > 8.5) || (loan.amount >= 50 && loan.rate > 6.5)) {
+  //   div.classList.add("current");
+  // }
+  // if ((loan.amount < 75 && loan.rate > 10) || (loan.amount >= 75 && loan.rate > 6.5)) {
+  //   div.classList.add("proposed");
+  // }
   figure.appendChild(div);
 });
 
-var steps = ["count", "clayton", "other", "current", "proposed", "clayton proposed"];
+var steps = ["count", "clayton", "other", "current", "proposed", "clayton proposed", "other proposed"];
 
 //world's simplest navigation
 document.body.addEventListener("click", function(e) {
   var step = e.target.getAttribute("data-goto");
   if (!step) return;
   document.body.setAttribute("data-step", step);
+  mode = step.indexOf("proposed") > -1 ? "proposed" : "current";
+  redraw();
 });
 
 document.body.addEventListener("click", function(e) {
@@ -69,5 +121,8 @@ document.body.addEventListener("click", function(e) {
   if (index == -1) return console.error("bad state!");
   index += increment * 1;
   if (index < 0 || index == steps.length) return;
-  document.body.setAttribute("data-step", steps[index]);
+  var step = steps[index];
+  mode = step.indexOf("proposed") > -1 ? "proposed" : "current";
+  redraw();
+  document.body.setAttribute("data-step", step);
 });
---
+++ tasks/exportBuckets.js
@@ -0,0 +1,76 @@
+module.exports = function(grunt) {
+
+  grunt.registerTask("export", function() {
+
+    var done = this.async();
+
+    var loans = grunt.data.csv.loans.filter(function(row) {
+      return row.rate;
+    });
+
+    var width = 61;
+    var rateDivision = 2;
+    var amountDivision = 5;
+
+    var matrix = [];
+    loans.forEach(function(loan) {
+      //rates are rounded up, because the conditions are always >
+      var rate = Math.ceil(loan.rate * rateDivision) / rateDivision;
+      var row = rate * rateDivision;
+      //amounts are rounded down, because their conditions are >=
+      var amount = Math.floor(loan.amount / amountDivision) * amountDivision;
+      var column = amount / amountDivision;
+      if (!matrix[row]) matrix[row] = new Array(width);
+      if (!matrix[row][column]) matrix[row][column] = {
+        rate: rate,
+        amount: amount,
+        clayton: 0,
+        other: 0,
+        count: 0
+      };
+      var counter = matrix[row][column];
+      counter.count++;
+      if (loan.company.trim().toLowerCase() == "clayton") {
+        counter.clayton++;
+      } else {
+        counter.other++;
+      }
+    });
+
+    var reduced = [];
+    for (var r = 0; r < matrix.length; r++) {
+      if (!matrix[r]) matrix[r] = new Array(width);
+      var arr = matrix[r];
+      for (var i = 0; i < arr.length; i++) {
+        if (!arr[i]) arr[i] = {
+          count: 0,
+          clayton: 0,
+          rate: r / rateDivision,
+          amount: i * amountDivision,
+          other: 0,
+          gap: true
+        }
+        reduced.push(arr[i]);
+      }
+    };
+
+    var bounds = {};
+    ["count", "clayton", "rate", "amount"].forEach(function(key) {
+      var bound = bounds[key] = {};
+      var values = reduced.filter(function(d) { return d.count }).map(function(d) { return d[key] });
+      bound.max = Math.max.apply(null, values);
+      bound.min = Math.min.apply(null, values);
+    });
+
+    var csv = require("csv");
+    var fs = require("fs");
+    console.log(Object.keys(reduced[0]));
+    csv.stringify(reduced, { header: true }, function(err, output) {
+      // console.log(output);
+      fs.writeFileSync("output.csv", output);
+      done();
+    });
+
+  });
+
+}
